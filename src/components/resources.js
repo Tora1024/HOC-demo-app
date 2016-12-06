1e3366ba25781d8e446702b74ec5172ea59693ac
Hector Coto Rojas <heavenhell64@gmail.com>
2016-12-06 09:07:40 -0600

    Some basic app structure here, Header and protected content as components, also set up a routing method to handle everything up.

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Super Special Secret Recipe
+			<ul>
+				<li>1 cup secret</li>
+				<li>1 cup shurikens</li>
+				<li>1 spoon super special sauce</li>
+			</ul>
+		</div>
+	);
+};
